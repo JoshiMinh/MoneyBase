792b2d6bcf82619ea95575423a996ce33b952fd7
Joshi Minh <43663567+JoshiMinh@users.noreply.github.com>
2025-09-01 13:57:03 +0700

    feat: implement pager indicator component

--- app/src/main/java/com/thebase/moneybase/screens/HomeScreen.kt
+++ app/src/main/java/com/thebase/moneybase/screens/HomeScreen.kt
@@ -42,7 +42,7 @@ import androidx.core.graphics.toColorInt
 import androidx.navigation.NavController
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.HorizontalPagerIndicator
+import com.thebase.moneybase.utils.components.HorizontalPagerIndicator
 import androidx.compose.foundation.pager.rememberPagerState
 import com.thebase.moneybase.database.Category
 import com.thebase.moneybase.database.FirebaseRepositories
